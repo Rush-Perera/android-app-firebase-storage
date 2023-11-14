efc65954582239975e21f7095afd8af7d4883b62
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-11-15 01:48:19 +0530

    initial and completed

---
+++ app/src/main/java/lk/rush/firebasestorage/ItemViewActivity.java
@@ -0,0 +1,14 @@
+package lk.rush.firebasestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_view);
+    }
+}
